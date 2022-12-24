3b7c5b0dcaa1035f0532cb1ac66776a25515d955
Vyacheslav Golovin <v.s.golovin@gmail.com>
2022-12-25 00:21:07 +0300

    Extract word waveforms

--- src/timit.py
+++ src/timit.py
@@ -1,6 +1,8 @@
-from typing import Union
+from typing import Optional, Sequence, Tuple, Union
 from pathlib import Path
+import librosa
 import pandas as pd
+import soundfile as sf
 
 
 PathLike = Union[Path, str]
@@ -11,7 +13,42 @@ class TimitCorpus:
         path = Path(path)
         assert path.exists() and path.is_dir(), f"Invalid path {path}"
         self.root = path
-        pass
+        # prompt_id -> prompt
+        self.prompts = read_prompts(self.root / "DOC/PROMPTS.TXT")
+        # speaker_id -> (prompt_ids)
+        self.spkrsent = read_spkrsent(self.root / "DOC/SPKRSENT.TXT")
+        # dataframe with speaker info
+        self.spkrinfo = read_spkrinfo(self.root / "DOC/SPKRINFO.TXT")
+
+    def _get_speaker_directory(self, speaker_id: str,
+                               info: Optional[pd.Series] = None) -> Path:
+        if info is None:
+            info = self.spkrinfo.loc[speaker_id]
+        subset_dir = self.root / ("TRAIN" if info["Use"] == "TRN" else "TEST")
+        dr_dir = subset_dir / f"DR{info['DR']}"
+        spkr_dir = dr_dir / (info["Sex"] + speaker_id)
+        assert spkr_dir.exists(), f"{spkr_dir} not found"
+        return spkr_dir
+
+    def split_common_sentences(self, save_to: PathLike) -> None:
+        # output directory
+        save_to = Path(save_to)
+        assert save_to.exists() and save_to.is_dir()
+
+        # get waveforms of words from SA1 and SA2 for every speaker
+        for spkr_id, spkr_info in self.spkrinfo.iterrows():
+            spkr_inp_dir = self._get_speaker_directory(spkr_id, spkr_info)
+            spkr_out_dir = save_to / spkr_id
+            spkr_out_dir.mkdir()
+            for pid in ("SA1", "SA2"):
+                wfm, sr = librosa.load(spkr_inp_dir / f"{pid}.WAV", sr=16000)
+                timestamps = read_wrd_file(spkr_inp_dir / f"{pid}.WRD")
+                for start, end, word in timestamps:
+                    sf.write(
+                        file=spkr_out_dir / f"{word.upper()}.WAV",
+                        data=wfm[start:end],
+                        samplerate=sr
+                    )
 
 
 def read_prompts(file: PathLike) -> dict:
@@ -89,3 +126,22 @@ def read_spkrinfo(file: PathLike) -> pd.DataFrame:
     assert len(data) == 630, "Wrong speaker count"
     df = pd.DataFrame(data=data, columns=columns)
     return df.set_index("ID")
+
+
+def read_wrd_file(file: PathLike) -> Sequence[Tuple[int, int, str]]:
+    """
+    Read a .WRD file with timestamps for every word in a recording.
+    """
+    timestamps = []
+    with open(file, "r") as fin:
+        for line in fin:
+            lst = line.rstrip().split()
+            if lst:
+                start, end, word = lst
+                timestamps.append((int(start), int(end), word))
+    return timestamps
+
+
+if __name__ == "__main__":
+    timit = TimitCorpus("data/TIMIT")
+    timit.split_common_sentences("data/words")
