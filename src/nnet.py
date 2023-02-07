d75f9783023e72dd667daf2857edccf996385352
Vyacheslav Golovin <v.s.golovin@gmail.com>
2023-02-07 22:13:18 +0300

    Add `Guesser`

--- src/nnet.py
+++ src/nnet.py
@@ -3,6 +3,52 @@ from torch import nn, Tensor
 from torch.nn import functional as F
 
 
+class Guesser(nn.Module):
+    def __init__(self, emb_dim: int = 128, dropout: float = 0.1,
+                 output_format: str = "logit"):
+        super().__init__()
+        hidden_dim = emb_dim * 2
+        self.attention = AdditiveAttention(emb_dim, hidden_dim, dropout)
+        self.mlp = nn.Sequential(
+            nn.Linear(hidden_dim, 2 * hidden_dim),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            nn.Linear(2 * hidden_dim, 1)
+        )
+        if output_format == "logit":
+            self.f_out = nn.Identity()
+        elif output_format == "logprob":
+            self.f_out = nn.LogSoftmax(dim=-2)
+        else:
+            assert output_format == "proba", \
+                f"Unknown output format {output_format}"
+            self.f_out = nn.Softmax(dim=-2)
+
+    def forward(self, X: Tensor, G: Tensor) -> Tensor:
+        """
+        Parameters
+        ----------
+        X : Tensor
+            word X-Vectors, shape (batch, T, d)
+        G : Tensor
+            speaker voice prints, shape (batch, K, d)
+
+        Notation same as in the paper: `T` is the number of word utterances
+        used for classification, `K` is the number of speakers in the current
+        pool, `d` is the embedding dimension.
+        """
+        T = X.size(1)
+        K = G.size(1)
+
+        # pool X-Vectors with attention conditioned on g_hat
+        g_hat = torch.mean(G, dim=1, keepdim=True)  # average over speakers
+        x_hat = self.attention(q=g_hat.repeat(1, T, 1), k=X, v=X)
+
+        # concatenate x_hat with guest voice print
+        logits = self.mlp(torch.cat([x_hat.repeat((1, K, 1)), G], dim=2))
+        return self.f_out(logits).squeeze(2)
+
+
 class AdditiveAttention(nn.Module):
     def __init__(self, input_dim: int = 128, hidden_dim: int = 256,
                  dropout: float = 0.1):
@@ -17,5 +63,5 @@ class AdditiveAttention(nn.Module):
     def forward(self, q: Tensor, k: Tensor, v: Tensor) -> Tensor:
         e = self.mlp(torch.cat([q, k], dim=-1))
         alpha = F.softmax(e, dim=-2)
-        v_hat = torch.sum(alpha * v, dim=-2)
+        v_hat = torch.sum(alpha * v, dim=-2, keepdim=True)
         return v_hat
--- tests/test_nnet.py
+++ tests/test_nnet.py
@@ -1,15 +1,26 @@
 import pytest
+import numpy as np
 import torch
 from src import nnet
 
 
-@pytest.mark.parametrize("t,d", [(3, 16), (5, 128)])
-def test_additive_attention(t: int, d: int):
-    b, t, d = 4, 5, 16
+@pytest.mark.parametrize("b,t,k,d", [(1, 3, 5, 16), (32, 5, 10, 128)])
+def test_guesser(b: int, t: int, k: int, d: int):
+    X = torch.randn((b, t, d))
+    G = torch.randn((b, k, d))
+    guesser = nnet.Guesser(emb_dim=d, output_format="proba")
+    probs = guesser(X, G)
+    prob_sums = probs.detach().sum(1).numpy()
+    assert probs.size() == torch.Size((b, k)) \
+        and np.allclose(prob_sums, np.ones(b))
+
+
+@pytest.mark.parametrize("b,t,d", [(1, 3, 16), (16, 5, 128)])
+def test_additive_attention(b: int, t: int, d: int):
     q = torch.zeros((b, t, d))
     k = torch.zeros((b, t, d))
     v = torch.zeros((b, t, d))
 
     att = nnet.AdditiveAttention(d, d * 2)
     v_hat = att(q, k, v)
-    assert v_hat.size() == torch.Size((b, d))
+    assert v_hat.size() == torch.Size((b, 1, d))
