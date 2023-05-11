189b7a51558019ace4d1f0875c9d580ad795f6ee
Vyacheslav Golovin <v.s.golovin@gmail.com>
2023-05-11 16:26:44 +0600

    Train `Verifier` with noise

--- src/verifier.py
+++ src/verifier.py
@@ -7,6 +7,7 @@ import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from isr.nnet import Verifier
 from isr import timit
+from isr.simple_agents import RandomAgent
 
 
 @click.group()
@@ -20,6 +21,8 @@ def cli():
 @click.option("--seed", type=int, default=2303, help="global seed")
 @click.option("--split-seed", type=int, default=42,
               help="seed used to perform train-val split")
+@click.option("-N", "--noise", is_flag=True, default=False,
+              help="whether to use noisy word recordings")
 @click.option("-T", "--num-words", type=int, default=3,
               help="number of words asked in every game")
 @click.option("--backend", type=click.Choice(["mlp", "cs"]), default="mlp",
@@ -32,12 +35,12 @@ def cli():
 @click.option("--lr", type=float, default=1e-4, help="learning rate")
 @click.option("--weight-decay", type=float, default=1e-4,
               help="L2 regularization")
-def train(sd_file: str, seed: int, split_seed: int, num_words: int,
-          backend: str, batch_size: int, iterations: int, max_epochs: int,
-          lr: float, weight_decay: float):
+def train(sd_file: str, seed: int, split_seed: int, noise: bool,
+          num_words: int, backend: str, batch_size: int, iterations: int,
+          max_epochs: int, lr: float, weight_decay: float):
     pl.seed_everything(seed)
     dm = XVectorsForVerifier(num_words, batch_size, iterations,
-                             seed=split_seed)
+                             seed=split_seed, noisy_words=noise)
     verifier = LitVerifier(backend, lr, weight_decay)
     early_stopping = EarlyStopping(monitor="val_loss", patience=5, mode="min")
     save_best = ModelCheckpoint(monitor="val_loss", mode="min", save_top_k=1,
@@ -61,6 +64,8 @@ def train(sd_file: str, seed: int, split_seed: int, num_words: int,
 @click.option("--seed", type=int, default=2303, help="global seed")
 @click.option("--split-seed", type=int, default=42,
               help="seed used to perform train-val split")
+@click.option("-N", "--noise", is_flag=True, default=False,
+              help="whether to use noisy word recordings")
 @click.option("-T", "--num-words", type=int, default=3,
               help="number of words asked in every game")
 @click.option("--backend", type=click.Choice(["mlp", "cs"]), default="mlp",
@@ -69,11 +74,12 @@ def train(sd_file: str, seed: int, split_seed: int, num_words: int,
 @click.option("--test-games", type=int, default=20000,
               help="total number of games (batch_size * iterations)")
 def test(all_subsets: bool, sd_file: str, seed: int, split_seed: int,
-         num_words: int, backend: str, batch_size: int, test_games: int):
+         noise: bool, num_words: int, backend: str, batch_size: int,
+         test_games: int):
     pl.seed_everything(seed)
     dm = XVectorsForVerifier(num_words, batch_size,
                              iterations_per_epoch=test_games//batch_size,
-                             seed=split_seed)
+                             seed=split_seed, noisy_words=noise)
     verifier = LitVerifier(backend)
     verifier.model.load_state_dict(torch.load(sd_file,
                                               map_location="cpu"))
@@ -137,17 +143,22 @@ class XVectorsForVerifier(pl.LightningDataModule):
     def __init__(self, num_words: int, batch_size: int,
                  iterations_per_epoch: int,
                  data_dir: Union[Path, str] = "./data", val_size: float = 0.2,
-                 seed: Optional[int] = None):
+                 seed: Optional[int] = None, noisy_words: bool = False):
         super().__init__()
         self.T = num_words
-        self.dset = timit.TimitXVectors(data_dir, val_size, seed)
+        self.dset = timit.TimitXVectors(data_dir, val_size, seed,
+                                        noisy_words=noisy_words)
         self.batch_size = batch_size
         self.iterations = iterations_per_epoch
+        self.agent = RandomAgent(total_words=self.dset.vocab_size)
 
     def _dataloader(self, subset: str) -> Generator:
         for _ in range(self.iterations):
-            g, x, targets = sample_games(self.dset, subset, self.batch_size,
-                                         num_words=self.T)
+            g, target_ids, targets = self.dset.sample_isv_games(
+                 self.batch_size, subset)
+            word_inds = self.agent.sample(num_envs=self.batch_size,
+                                          num_words=self.T)
+            x = self.dset.get_word_embeddings(target_ids, word_inds)
             yield g, x, targets
 
     def train_dataloader(self):
